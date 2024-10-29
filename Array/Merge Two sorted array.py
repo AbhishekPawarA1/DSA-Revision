72afa699c9b6af89709754f14af03ba30cad8f70
AbhishekA1 <abhishekanandpawar1@gmail.com>
2024-10-29 20:27:40 +0530

    Bubble Sort

--- Array/Merge Two sorted array.py
+++ Array/Merge Two sorted array.py
@@ -3,3 +3,16 @@
 # Example:
 # Input: arr1 = [1, 3, 5], arr2 = [2, 4, 6]
 # Output: [1, 2, 3, 4, 5, 6]
+
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+array=arr1+arr2;
+n=len(array)
+for i in range(n):
+    for j in range(n-i-1):
+        if array[j]>array[j+1]:
+            array[j],array[j+1]=array[j+1],array[j]
+print(array)        
+
+        
+
