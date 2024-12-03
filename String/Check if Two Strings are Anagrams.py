6c902bc979d69ba9b19bf4bad34aa5784673fef0
AbhishekA1 <abhishekanandpawar1@gmail.com>
2024-12-03 10:49:40 +0530

    anagram check

--- String/Check if Two Strings are Anagrams.py
+++ String/Check if Two Strings are Anagrams.py
@@ -8,3 +8,20 @@
 # Input: "hello", "world"
 # Output: False
 
+arr1="listen"
+arr2="silent"
+isanagram=True
+
+
+if len(arr1) == len(arr2):  
+    for char in arr1:
+        if arr1.count(char) != arr2.count(char):  # Compare character frequencies
+            isanagram = False
+            break
+else:
+    isanagram = False  
+
+if isanagram:
+    print("True")
+else:
+    print("False")        
